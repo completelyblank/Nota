595ba1c02eab3630b9fa1688832ac6b0f77b9d91
Muhammad Raza <105001837+completelyblank@users.noreply.github.com>
2025-06-19 16:27:40 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   experimental: {
-    serverActions: true,
+    serverActions: false,
   },
   // Other config options
 };
